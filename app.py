334fd0f7ef0964e7a1c9080e9e4f1d522ccbdb2b
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-12-02 16:52:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def main():
 
     # Sidebar
     st.sidebar.header("Options")
-    option = st.sidebar.selectbox("Select an option", ["Add City", "Set Start City", "Set Cost Matrix", "Solve TSP"])
+    option = st.sidebar.selectbox("Select an option", ["Add City", "Set Start City", "Set Cost Matrix"])
 
     # Main content
     if option == "Add City":
@@ -93,25 +93,24 @@ def main():
                 else:
                     st.warning("Please add cities first.")
 
-            if tsp_solver.start_city:
-                if st.button("Solve TSP"):
-                    try:
-                        result, cost = tsp_solver.solve_tsp()
-                        route = ' -> '.join(result)
-                        st.subheader("Optimal Path:")
-                        st.write(route)
-                        st.subheader("Total Cost:")
-                        st.write(cost)
+            if st.button("Solve TSP"):
+                try:
+                    result, cost = tsp_solver.solve_tsp()
+                    route = ' -> '.join(result)
+                    st.subheader("Optimal Path:")
+                    st.write(route)
+                    st.subheader("Total Cost:")
+                    st.write(cost)
 
-                        # Option to calculate legs
-                        calculate_legs = st.checkbox("Calculate Legs")
-                        if calculate_legs:
-                            legs = len(result) - 1
-                            st.subheader("Number of Legs:")
-                            st.write(legs)
+                    # Option to calculate legs
+                    calculate_legs = st.checkbox("Calculate Legs")
+                    if calculate_legs:
+                        legs = len(result) - 1
+                        st.subheader("Number of Legs:")
+                        st.write(legs)
 
-                    except ValueError as e:
-                        st.error(str(e))
+                except ValueError as e:
+                    st.error(str(e))
 
     # Display added cities
     if tsp_solver.session_state.cities:
