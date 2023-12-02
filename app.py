6eea6525d1ed32a478abcd70ea75539047dbdce1
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-12-02 20:18:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def main():
     st.sidebar.subheader("Options")
     option = st.sidebar.selectbox("Select an option", ["Add City", "Set Cost Matrix"])
 
-    # Display added cities as a matrix
+    # Display added cities as a matrix and set cost matrix
     if tsp_solver.cities:
         st.subheader("Added Cities Matrix:")
         st.write(create_matrix_table(tsp_solver.cities))
@@ -99,45 +99,44 @@ def main():
 
         elif option == "Set Cost Matrix":
             st.subheader("Set Cost Matrix")
-            if tsp_solver.cities:
-                tsp_solver.cost_matrix = create_matrix_table(tsp_solver.cities)
-                st.table(tsp_solver.cost_matrix)
+            tsp_solver.cost_matrix = create_matrix_table(tsp_solver.cities)
+            st.table(tsp_solver.cost_matrix)
 
-                # Allow user to input costs in the matrix
-                for i in range(len(tsp_solver.cities)):
-                    for j in range(i + 1, len(tsp_solver.cities)):
-                        cost = st.number_input(f"Enter cost between {tsp_solver.cities[i]} and {tsp_solver.cities[j]}:")
-                        tsp_solver.set_cost(tsp_solver.cities[i], tsp_solver.cities[j], cost)
+            # Allow user to input costs in the matrix
+            for i in range(len(tsp_solver.cities)):
+                for j in range(i + 1, len(tsp_solver.cities)):
+                    cost = st.number_input(f"Enter cost between {tsp_solver.cities[i]} and {tsp_solver.cities[j]}:")
+                    tsp_solver.set_cost(tsp_solver.cities[i], tsp_solver.cities[j], cost)
 
-                if st.button("Set Start City"):
-                    if tsp_solver.cities:
-                        start_city = st.selectbox("Select start city:", tsp_solver.cities)
-                        try:
-                            tsp_solver.set_start_city(start_city)
-                            st.success(f"Start city set to '{start_city}' successfully!")
-                        except ValueError as e:
-                            st.error(str(e))
-                    else:
-                        st.warning("Please add cities first.")
-
-                if st.button("Solve TSP"):
+            if st.button("Set Start City"):
+                if tsp_solver.cities:
+                    start_city = st.selectbox("Select start city:", tsp_solver.cities)
                     try:
-                        result, cost = tsp_solver.solve_tsp()
-                        route = ' -> '.join(result)
-                        st.subheader("Optimal Path:")
-                        st.write(route)
-                        st.subheader("Total Cost:")
-                        st.write(cost)
-
-                        # Option to calculate legs
-                        calculate_legs = st.checkbox("Calculate Legs")
-                        if calculate_legs:
-                            legs = len(result) - 1
-                            st.subheader("Number of Legs:")
-                            st.write(legs)
-
+                        tsp_solver.set_start_city(start_city)
+                        st.success(f"Start city set to '{start_city}' successfully!")
                     except ValueError as e:
                         st.error(str(e))
+                else:
+                    st.warning("Please add cities first.")
+
+            if st.button("Solve TSP"):
+                try:
+                    result, cost = tsp_solver.solve_tsp()
+                    route = ' -> '.join(result)
+                    st.subheader("Optimal Path:")
+                    st.write(route)
+                    st.subheader("Total Cost:")
+                    st.write(cost)
+
+                    # Option to calculate legs
+                    calculate_legs = st.checkbox("Calculate Legs")
+                    if calculate_legs:
+                        legs = len(result) - 1
+                        st.subheader("Number of Legs:")
+                        st.write(legs)
+
+                except ValueError as e:
+                    st.error(str(e))
 
 if __name__ == "__main__":
     main()
