4f08ecee35a1dff214f58e1d2d65ff9bb6058c9d
Kerubo Decra <90448048+keruboDecra@users.noreply.github.com>
2023-12-02 17:02:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def main():
 
     # Sidebar
     st.sidebar.header("Options")
-    option = st.sidebar.selectbox("Select an option", ["Add City", "Set Start City", "Set Cost Matrix"])
+    option = st.sidebar.selectbox("Select an option", ["Add City", "Set Start City", "Set Matrix Cost", "Solve TSP"])
 
     # Main content
     if option == "Add City":
@@ -71,10 +71,9 @@ def main():
             tsp_solver.add_city(city)
             st.success(f"City '{city}' added successfully!")
 
-    elif option == "Set Cost Matrix":
+    elif option == "Set Matrix Cost":
         if tsp_solver.session_state.cities:
             tsp_solver.cost_matrix = create_matrix_table(tsp_solver.session_state.cities)
-            st.table(tsp_solver.cost_matrix)
 
             # Allow user to input costs in the matrix
             for i in range(len(tsp_solver.session_state.cities)):
@@ -93,24 +92,28 @@ def main():
                 else:
                     st.warning("Please add cities first.")
 
-            if st.button("Solve TSP"):
-                try:
-                    result, cost = tsp_solver.solve_tsp()
-                    route = ' -> '.join(result)
-                    st.subheader("Optimal Path:")
-                    st.write(route)
-                    st.subheader("Total Cost:")
-                    st.write(cost)
+            if tsp_solver.start_city:
+                st.subheader("Cost Matrix:")
+                st.table(tsp_solver.cost_matrix.values.tolist())  # Convert DataFrame to a simple representation
 
-                    # Option to calculate legs
-                    calculate_legs = st.checkbox("Calculate Legs")
-                    if calculate_legs:
-                        legs = len(result) - 1
-                        st.subheader("Number of Legs:")
-                        st.write(legs)
+                if st.button("Solve TSP"):
+                    try:
+                        result, cost = tsp_solver.solve_tsp()
+                        route = ' -> '.join(result)
+                        st.subheader("Optimal Path:")
+                        st.write(route)
+                        st.subheader("Total Cost:")
+                        st.write(cost)
 
-                except ValueError as e:
-                    st.error(str(e))
+                        # Option to calculate legs
+                        calculate_legs = st.checkbox("Calculate Legs")
+                        if calculate_legs:
+                            legs = len(result) - 1
+                            st.subheader("Number of Legs:")
+                            st.write(legs)
+
+                    except ValueError as e:
+                        st.error(str(e))
 
     # Display added cities
     if tsp_solver.session_state.cities:
